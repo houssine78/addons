f58c1e4599faee23e2269dea367700903e1ca4cc
nicolasjamoulle <nicolasjamoulle@yahoo.fr>
2019-02-11 10:32:54 +0100

    Fix review

--- project_remaining_time_on_task/models/project.py
+++ project_remaining_time_on_task/models/project.py
@@ -14,13 +14,13 @@ class Task(models.Model):
         digits=(16, 2))
 
     def _get_qty_invoiced(self):
-        _qty_invoiced = 0
+        qty_invoiced = 0
         sale_order = self.env['sale.order'].search(
             [('project_id', '=', self.analytic_account_id.id)])
         for sale_order_lines in sale_order.order_line:
-            _qty_invoiced += sum([line.qty_invoiced for line in sale_order_lines])
+            qty_invoiced += sum([line.qty_invoiced for line in sale_order_lines])
 
-        return _qty_invoiced
+        return qty_invoiced
 
     def _get_qty_effective(self):
         tasks = self.project_id.task_ids
@@ -30,18 +30,19 @@ class Task(models.Model):
     @api.multi
     @api.depends('remaining_hours')
     def _compute_remaining_hours_contract(self):
-        _qty_invoiced = self._get_qty_invoiced()
-        if _qty_invoiced > 0:
-            for task in self:
-                task.remaining_contract_hours = _qty_invoiced \
-                                                - self._get_qty_effective()
+        for sale_order in self:
+            sale_order.qty_invoiced = self._get_qty_invoiced()
+            if sale_order.qty_invoiced > 0:
+                for task in self:
+                    task.remaining_contract_hours = sale_order.qty_invoiced \
+                                                    - self._get_qty_effective()
 
     @api.multi
     @api.depends('has_contract_hours')
     def _compute_has_contract_hours(self):
-        _qty_invoiced = 0
+        qty_invoiced = 0
         sale_order = self.env['sale.order'].search(
             [('project_id', '=', self.analytic_account_id.id)])
         for sale_order_lines in sale_order.order_line:
-            _qty_invoiced += sum([line.qty_invoiced for line in sale_order_lines])
-            self.has_contract_hours = _qty_invoiced > 0
+            qty_invoiced += sum([line.qty_invoiced for line in sale_order_lines])
+            self.has_contract_hours = qty_invoiced > 0
