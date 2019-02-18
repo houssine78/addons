4d0a5623d156e0962953b6bcf0ea7d0b9beb2202
nicolasjamoulle <nicolasjamoulle@yahoo.fr>
2019-02-18 13:10:37 +0100

    fix review

--- project_remaining_time_on_task/models/project.py
+++ project_remaining_time_on_task/models/project.py
@@ -13,35 +13,47 @@ class Task(models.Model):
         compute='_compute_remaining_hours_contract',
         digits=(16, 2))
 
-    def _get_qty_invoiced(self):
-        qty_invoiced = 0
-        sale_order = self.env['sale.order'].search(
-            [('project_id', '=', self.analytic_account_id.id)])
-        for sale_order_lines in sale_order.order_line:
-            qty_invoiced = sum([line.qty_invoiced for line in sale_order_lines])
+    # @api.multi
+    # def _get_qty_invoiced(self):
+    #     self.ensure_one()
+    #     sale_order = self.env['sale.order'].search(
+    #         [('project_id', '=', self.analytic_account_id.id)])
+    #     return sum(line.qty_invoiced for line in sale_order.order_line)
 
-        return qty_invoiced
-
+    @api.multi
     def _get_qty_effective(self):
+        self.ensure_one()
         tasks = self.project_id.task_ids
 
         return sum([task.effective_hours for task in tasks])
 
+    # @api.multi
+    # @api.depends('remaining_hours')
+    # def _compute_remaining_hours_contract(self):
+    #     for task in self:
+    #         sale_order = self.env['sale.order'].search(
+    #         [('project_id', '=', self.analytic_account_id.id)])
+    #         sale_order.qty_invoiced = sale_order._get_qty_invoiced()
+    #         if sale_order.qty_invoiced > 0:
+    #             for task in self:
+    #                 task.remaining_contract_hours = sale_order.qty_invoiced \
+    #                                                 - task._get_qty_effective()
+
     @api.multi
     @api.depends('remaining_hours')
     def _compute_remaining_hours_contract(self):
-        for sale_order in self:
-            sale_order.qty_invoiced = self._get_qty_invoiced()
-            if sale_order.qty_invoiced > 0:
-                for task in self:
-                    task.remaining_contract_hours = sale_order.qty_invoiced \
-                                                    - self._get_qty_effective()
+        sale_order = self.env['sale.order'].search(
+            [('project_id', '=', self.analytic_account_id.id)])
+        qty_invoiced = sum(line.qty_invoiced for line in sale_order.order_line)
+        for task in self:
+            task.remaining_contract_hours = qty_invoiced \
+                                            - task._get_qty_effective()
 
     @api.multi
     @api.depends('has_contract_hours')
     def _compute_has_contract_hours(self):
+        self.ensure_one()
         sale_order = self.env['sale.order'].search(
             [('project_id', '=', self.analytic_account_id.id)])
-        for sale_order_lines in sale_order.order_line:
-            qty_invoiced = sum([line.qty_invoiced for line in sale_order_lines])
-            self.has_contract_hours = qty_invoiced > 0
+        qty_invoiced = sum(line.qty_invoiced for line in sale_order.order_line)
+        self.has_contract_hours = qty_invoiced > 0
