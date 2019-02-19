116fcdf79cfbea9f06e1a3cf06098a895c542869
nicolasjamoulle <nicolasjamoulle@yahoo.fr>
2019-02-19 15:43:50 +0100

    Fix review

--- project_remaining_time_on_task/models/project.py
+++ project_remaining_time_on_task/models/project.py
@@ -13,13 +13,6 @@ class Task(models.Model):
         compute='_compute_remaining_hours_contract',
         digits=(16, 2))
 
-    # @api.multi
-    # def _get_qty_invoiced(self):
-    #     self.ensure_one()
-    #     sale_order = self.env['sale.order'].search(
-    #         [('project_id', '=', self.analytic_account_id.id)])
-    #     return sum(line.qty_invoiced for line in sale_order.order_line)
-
     @api.multi
     def _get_qty_effective(self):
         self.ensure_one()
@@ -27,27 +20,15 @@ class Task(models.Model):
 
         return sum([task.effective_hours for task in tasks])
 
-    # @api.multi
-    # @api.depends('remaining_hours')
-    # def _compute_remaining_hours_contract(self):
-    #     for task in self:
-    #         sale_order = self.env['sale.order'].search(
-    #         [('project_id', '=', self.analytic_account_id.id)])
-    #         sale_order.qty_invoiced = sale_order._get_qty_invoiced()
-    #         if sale_order.qty_invoiced > 0:
-    #             for task in self:
-    #                 task.remaining_contract_hours = sale_order.qty_invoiced \
-    #                                                 - task._get_qty_effective()
-
     @api.multi
     @api.depends('remaining_hours')
     def _compute_remaining_hours_contract(self):
+        self.ensure_one()
         sale_order = self.env['sale.order'].search(
             [('project_id', '=', self.analytic_account_id.id)])
         qty_invoiced = sum(line.qty_invoiced for line in sale_order.order_line)
-        for task in self:
-            task.remaining_contract_hours = qty_invoiced \
-                                            - task._get_qty_effective()
+        self.remaining_contract_hours = (qty_invoiced
+                                         - self._get_qty_effective())
 
     @api.multi
     @api.depends('has_contract_hours')
